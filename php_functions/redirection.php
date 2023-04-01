b628bf5c6f79502388b0ff5220ce21c2070c2be1
fanfanafankianki <kudlatyicss@gmail.com>
2023-04-01 23:36:55 +0200

    Changes in ecs for test

--- php_functions/redirection.php
+++ php_functions/redirection.php
@@ -1,13 +1,13 @@
 <?php
 function redirectToWelcomePage() {
-    $url = "http://localhost/welcome.php";
+    $url = "http://" . $_SERVER['HTTP_HOST'] . "/welcome.php";
     header("Location: $url");
     exit;
 }
 
 
 function redirectToLoggedPage() {
-    $url = "http://localhost/logged.php";
+    $url = "http://" . $_SERVER['HTTP_HOST'] . "/logged.php";
     header("Location: $url");
     exit;
 }
