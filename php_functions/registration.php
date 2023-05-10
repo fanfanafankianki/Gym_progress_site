8dcf2adcbb317f37625c83da88e7b652cd18e74c
fanfanafankianki <kudlatyicss@gmail.com>
2023-05-10 15:44:48 +0200

    Registration should work now

--- php_functions/registration.php
+++ php_functions/registration.php
@@ -15,37 +15,48 @@ if(isset($_POST['submitRegistration'])) {
 
     $conn = connectToDbUsers();
 
-    // Wstawianie danych do bazy danych
-    $sql = "INSERT INTO accounts (username, email, password)
-    VALUES ('$hashed_username', '$hashed_email', '$hashed_password')";
+    // Sprawdzanie unikalności użytkownika
+    $sql_check = "SELECT * FROM accounts WHERE username='$hashed_username' OR email='$hashed_email'";
+    $result_check = mysqli_query($conn, $sql_check);
 
-    if (mysqli_query($conn, $sql)) {
-        echo "Użytkownik zarejestrowany pomyślnie";
-        
-        // Pobranie ostatniego ID z tabeli accounts
-        $last_id = mysqli_insert_id($conn);
-        
-        // Tworzenie połączenia
-        $conn2 = connectToDb();
-
+    if (mysqli_num_rows($result_check) == 0) {
         // Wstawianie danych do bazy danych
-        $sql = "INSERT INTO users (user_id, user_name)
-        VALUES ('$last_id', '$username')";
+        $sql = "INSERT INTO accounts (username, email, password)
+        VALUES ('$hashed_username', '$hashed_email', '$hashed_password')";
 
-        if (mysqli_query($conn2, $sql)) {
-            echo "Użytkownik dodany pomyślnie";
+        if (mysqli_query($conn, $sql)) {
+            echo "Użytkownik zarejestrowany pomyślnie";
+            
+            // Pobranie ostatniego ID z tabeli accounts
+            $last_id = mysqli_insert_id($conn);
+            
+            // Tworzenie połączenia
+            $conn2 = connectToDb();
+
+            // Wstawianie danych do bazy danych
+            $sql = "INSERT INTO users (user_id, user_name)
+            VALUES ('$last_id', '$username')";
+
+            if (mysqli_query($conn2, $sql)) {
+                echo "Użytkownik dodany pomyślnie";
+            } else {
+                echo "Błąd podczas rejestracji: " . mysqli_error($conn2);
+                $error_registration = "Błąd podczas rejestracji: " . mysqli_error($conn2);
+                $_SESSION['error_registration'] = $error_registration;  
+                echo $error_registration;
+            }
+
+            mysqli_close($conn2);
+
         } else {
-            echo "Błąd podczas rejestracji: " . mysqli_error($conn2);
-            $error_registration = "Błąd podczas rejestracji: " . mysqli_error($conn2);
+            echo "Błąd podczas rejestracji: " . mysqli_error($conn);
+            $error_registration = "Błąd podczas rejestracji: Nazwa użytkownika nie może być dłuższa niż 20 znaków. Email musi być unikalny. Hasło nie może być dłuższe niz 20 znaków.";
             $_SESSION['error_registration'] = $error_registration;  
             echo $error_registration;
         }
-
-        mysqli_close($conn2);
-
     } else {
-        echo "Błąd podczas rejestracji: " . mysqli_error($conn);
-        $error_registration = "Błąd podczas rejestracji: Nazwa użytkownika nie może być dłuższa niż 20 znaków. Email musi być unikalny. Hasło nie może być dłuższe niz 20 znaków.";
+        echo "Użytkownik o podanej nazwie użytkownika lub adresie e-mail już istnieje.";
+        $error_registration = "Użytkownik o podanej nazwie użytkownika lub adresie e-mail już istnieje.";
         $_SESSION['error_registration'] = $error_registration;  
         echo $error_registration;
     }
